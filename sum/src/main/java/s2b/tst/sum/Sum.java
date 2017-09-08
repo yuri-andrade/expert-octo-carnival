1d67db1daee33e2ee217e4fdd0362780f5f79c84
Marco Mangan <marco.mangan@pucrs.br>
2017-09-08 15:53:58 -0300

    Comentários e mensagens de exceção.

--- sum/src/main/java/s2b/tst/sum/Sum.java
+++ sum/src/main/java/s2b/tst/sum/Sum.java
@@ -1,13 +1,24 @@
 package s2b.tst.sum;
 
+/**
+ * 
+ * @author marco.mangan@pucrs.br
+ *
+ */
 public class Sum {
 
+	/**
+	 * 
+	 * @param d
+	 * @param e
+	 * @return
+	 */
 	public static double sum(double d, double e) {	
 		if (d < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O primeiro número não pode ser negativo!");
 		}
 		if (e < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O segundo número não pode ser negativo!");
 		}
 		return d + e;
 	}
