60fbee932f4a33deb5c5bc5aefc0e4a32abafa8d
yuri-andrade <yuri.arend@acad.pucrs.br>
2017-09-12 15:11:03 -0300

    Entrada de dados do usuario

--- sum/src/main/java/s2b/tst/sum/App.java
+++ sum/src/main/java/s2b/tst/sum/App.java
@@ -1,5 +1,7 @@
 package s2b.tst.sum;
+import java.util.Scanner;
 
+import com.sun.corba.se.spi.orbutil.fsm.Input;
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        System.out.println("A:");
+        double a = in.nextDouble();
+        System.out.println("B:");
+        double b = in.nextDouble();
+        in.close();
+        double r=Sum.sum(a, b);
+        System.out.println("A:"+a);
+        System.out.println("B:"+b);
+        System.out.println("Soma:"+r);
     }
 }
